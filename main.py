bd5a5a475b84555bf0b2d26c761dc0b0faa09b3d
PoonamKalita18 <arunakalita2015@gmail.com>
2024-11-07 21:27:46 +0530

    dfsd

---
+++ main.py
@@ -0,0 +1,14 @@
+class student:
+    grade = 11
+    name ='Poonam'
+
+def introduction(self):
+    print('hi i am a student')
+
+def details(self):
+    print('my name is ',self.name)
+    print('I study in grade ', grade)
+
+ob = student()
+ob.introduction()
+ob.details()
